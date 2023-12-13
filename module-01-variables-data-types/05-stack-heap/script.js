8d1a4031181f2da2531e49f56b31c28ade7f8f9e
Aidunlin <aidunlin@gmail.com>
2023-12-13 13:58:59 -0800

    Initial commit

---
+++ module-01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,18 @@
+// Value is stored in the stack
+// const name = 'John';
+// const age = 30;
+
+// Reference is stored in the heap
+// const person = {
+//   name: 'Bruce',
+//   age: 59,
+// };
+
+// let newName = name;
+// newName = 'Jonathan';
+
+// let newPerson = person;
+// newPerson.name = 'Bruce Wayne';
+
+// console.log(name, newName); // John, Jonathan
+// console.log(person, newPerson); // { name: 'Bradley', age: 40 }, { name: 'Bradley', age: 40 }
